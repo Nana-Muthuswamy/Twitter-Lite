a19fe7b2590ddf75ebc112471c0b6ffdec9b67eb
Nana <narayanan_eie@yahoo.com>
2017-04-14 23:41:31 -0700

    Designed the Login View and added App icons

--- TwitterLite/LoginViewController.swift
+++ TwitterLite/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  TwitterLite
 //
 //  Created by Nana on 4/14/17.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
